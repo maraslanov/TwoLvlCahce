9d4e5377606094558896e043713512728ea54a73
Арасланов Максим <maraslanov@diasoft.ru>
2018-11-22 17:47:35 +0500

    add correcting configs

--- src/main/java/com/example/twolvlcahce/web/config/SwaggerConfig.java
+++ src/main/java/com/example/twolvlcahce/web/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.example.twolvlcahce.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -13,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        //Configuring Swagger 2 in the Application
+        //Configuring Swagger in the Application
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.twolvlcahce.web.controller"))
--- src/main/java/com/example/twolvlcahce/web/config/WebMvcConfig.java
+++ src/main/java/com/example/twolvlcahce/web/config/WebMvcConfig.java
@@ -2,10 +2,10 @@ package com.example.twolvlcahce.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter
+public class WebMvcConfig implements WebMvcConfigurer
 {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
